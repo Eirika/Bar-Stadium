77d6094ba70ae16db0dcfa064e1b690190b4f0db
Eirika <pitchetrou@msn.com>
2014-05-11 01:24:12 +0200

    Update Model

--- bar_stadium/shop/models.py
+++ bar_stadium/shop/models.py
@@ -5,17 +5,17 @@ class Produit(models.Model):
 	nom = models.CharField(max_length=50)
 	descri = models.TextField()
 	prix = models.IntegerField()
-	urlImg = models.TextField
+	urlImg = models.TextField()
+
 
 class Boisson(Produit):
 	degre = models.IntegerField()
 	alcool = models.BooleanField(default=False)
 
+		
 class Glace(Produit):
-	# def __init__(self, arg):
-	# 	super(Glace,Produit).__init__()
-	# 	self.arg = arg
-	parfum = CharField(max_length=50)
+	parfum = models.CharField(max_length=50)
+
 
 class Commande(models.Model):
 	prixTTC = models.DecimalField(max_digits=6, decimal_places=2)
@@ -25,14 +25,17 @@ class Commande(models.Model):
 	loge = models.ForeignKey('Loge')
 	produit = models.ManyToManyField(Produit, through='LigneCom')
 
+
 class LigneCom(models.Model):
 	quantite = models.IntegerField()
 	produit = models.ForeignKey(Produit)
 	commande = models.ForeignKey(Commande)
 
+
 class Serveur(models.Model):
 	nom = models.CharField(max_length=50)
 	prenom = models.CharField(max_length=50)
+
 
 class Loge(models.Model):
 	libelle = models.CharField(max_length=50)
